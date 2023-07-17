ed0932aa810b19f635b31e091e4e48e16ffd2c31
rc2pc2 <petriccarcc@gmail.com>
2023-07-17 11:00:29 +0200

    example of an elegant class solution

--- models/Food.php
+++ models/Food.php
@@ -4,13 +4,32 @@ include_once __DIR__ . '/Product.php';
 
 
 class Food extends Product {
-    public $calories;
-    public $fats;
+    protected $calories;
+    protected $fats;
 
     function __construct(String $name,String $description,Float $price, String $image, Bool $inStock, Category $category, Int $quantity, Int $calories, Int $fats){
         parent::__construct($name, $description, $price, $image, $inStock, $category, $quantity);
-        $this->calories = $calories;
-        $this->fats = $fats;
+        $this->setCalories($calories);
+        $this->setFats($fats);
     }
 
+    public function getCalories() : Int {
+        return $this->calories;
+    }
+
+    public function getFats() : Int {
+        return $this->fats;
+    }
+
+    private function setCalories(Int $calories) : void {
+        if ($calories < 0){
+            $calories = 0;
+        }
+
+        $this->calories = $calories;
+    }
+
+    private function setFats(Int $fats) : void {
+        $this->calories = $fats;
+    }
 }
